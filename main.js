63f52e61144c3e0304d20edb0f27f7661764c20f
이서연 <iseoyeon@iseoyeon-ui-MacBookPro.local>
2024-10-01 23:52:09 +0900

    2nd

--- main.js
+++ main.js
@@ -3,32 +3,34 @@ document.getElementById('time').innerHTML = new Date().toLocaleString()
 function pink() {
   document.body.style.backgroundColor = 'pink'
 }
-function yellow() {
-  document.body.style.backgroundColor = 'yellow'
+function green() {
+  document.body.style.backgroundColor = 'lightgreen'
 }
 function lightblue() {
   document.body.style.backgroundColor = 'lightblue'
 }
 function reset() {
-  document.body.style.backgroundColor = 'white'
+  document.body.style.backgroundColor = 'beige'
 }
 
 function showhtml() {
-  document.getElemetById('fig').src = 'img/html5.jpg'
-  document.getElemetById('desc').innerHTML =
+  document.getElementById('fig').src = 'img/html5.jpg'
+  document.getElementById('desc').innerHTML =
     '<b>HTML</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
 }
 function showcss() {
-  document.getElemetById('fig').src = 'img/css3.jpg'
-  document.getElemetById('desc').innerHTML =
+  document.getElementById('fig').src = 'img/css3.jpg'
+  document.getElementById('desc').innerHTML =
     '<b>CSS</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
 }
 function showjavascript() {
-  document.getElemetById('fig').src = 'img/javascript.jpg'
-  document.getElemetById('desc').innerHTML =
+  document.getElementById('fig').src = 'img/javascript.jpg'
+  document.getElementById('desc').innerHTML =
     '<b>Javascript</b>은 구조적 웹문서를 작성하는데 사용하는 마크업 언어이다'
 }
 function Hide() {
-  document.getElemetById('fig').src = ' '
-  document.getElemetById('desc').innerHTML = ' '
+  document.getElementById('fig').src = ' '
+  document.getElementById('desc').innerHTML = ' '
 }
+
+function b() {}
